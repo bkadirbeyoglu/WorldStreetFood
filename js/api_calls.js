7a9290973d5eeb7d73e4b7da1f24a6f2edace737
Burak K <bkadirbeyoglu@gmail.com>
2021-11-14 16:37:10 +0300

    ...

--- js/api_calls.js
+++ js/api_calls.js
@@ -1,6 +1,8 @@
 const ACCESS_KEY = "94145170-a02c-430c-afd257165891-d3a1-44cf";
 const LIBRARY_ID = 13766;
 
+//http://rokuawgserver.ddns.net/ctv/channels/1/roku.xml
+
 const OPTIONS = {
     method: "GET",
     headers: {
@@ -10,28 +12,48 @@ const OPTIONS = {
 };
 
 let collections = [];
+let channelXml;
 
+
+function getChannelXML() {
+    let promise = new Promise(function(resolve, reject) {
+        getXML("http://rokuawgserver.ddns.net/ctv/channels/1/roku.xml", function (error, data) {
+            if (error !== null) {
+                console.log(error);
+                reject(error);
+            }
+            else {
+                console.log(data);
+                resolve(data);
+            }
+        });
+    });
+
+    return promise;
+}
 
 function getCollectionList() {
     const url = `http://video.bunnycdn.com/library/${LIBRARY_ID}/collections??page=1&itemsPerPage=100&orderBy=date`;
 
-    fetch(url, OPTIONS)
-    .then((response) => {
-        if (response.status >= 200 && response.status <= 299) {
-            return response.json();
-        } 
-        else {
-            throw Error(response.statusText);
-        }
-    })
-    .then(response => {
-        //console.log(response);
-        collections = response.items;
-        if (collections.length > 0) {
-            getVideosInCollections();
-        }
-    })
-    .catch(err => console.error(err));
+    return new Promise(function (resolve, reject) {
+        fetch(url, OPTIONS)
+        .then((response) => {
+            if (response.status >= 200 && response.status <= 299) {
+                return response.json();
+            } 
+            else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(response => {
+            //console.log(response);
+            collections = response.items;
+            if (collections.length > 0) {
+                getVideosInCollections();
+            }
+        })
+        .catch(err => console.error(err));
+    });
 }
 
 
@@ -66,11 +88,19 @@ function getVideosInCollections() {
                 collections[index].videos = res.value.items;
             }
         });
-        //console.log(collections);
+        console.log(collections);
 
-        document.getElementById("splash-screen").classList.add("hidden");
+        getChannelXML().then(function(xmlData) {
+            console.log(xmlData);
+
+            document.getElementById("splash-screen").classList.add("hidden");
+            document.getElementById("collections-screen").classList.remove("hidden");
+            buildCollectionsScreen();
+        });
+
+        /* document.getElementById("splash-screen").classList.add("hidden");
         document.getElementById("collections-screen").classList.remove("hidden");
-        buildCollectionsScreen();
+        buildCollectionsScreen(); */
     });
 }
 
@@ -94,4 +124,19 @@ if (!Promise.allSettled) {
 		const convertedPromises = promises.map(p => Promise.resolve(p).then(resolveHandler, rejectHandler));
 		return Promise.all(convertedPromises);
 	};
+}
+
+
+function getXML(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url, true);
+	xhr.onload = function () {
+		if (xhr.status === 200) {
+			callback(null, xhr.response);
+		}
+		else {
+			callback(xhr.status, xhr.responseText);
+		}
+	};
+	xhr.send();
 }
