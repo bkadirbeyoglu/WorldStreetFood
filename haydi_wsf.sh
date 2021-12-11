64768c312ec214120f3f7f31b26665b891ac529d
burak.kadirbeyoglu <>
2021-12-12 01:38:59 +0300

    ...

--- haydi_wsf.sh
+++ haydi_wsf.sh
@@ -2,7 +2,7 @@ cd $HOME/workspace/WSF
 tizen build-web
 cd .buildResult
 rm -r .git
-tizen package -t wgt -s bkdmobile_tv
+tizen package -t wgt -s bkdmobile_tv_2
 tizen install -s 192.168.1.32:26101 -n WSF.wgt
 OUTPUT=$(sdb -s 192.168.1.32:26101 shell 0 debug fgadnv6P3J.WSF)
 LEN=${#OUTPUT}
